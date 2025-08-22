9748a43d6d7720084227fb4838d120cfe6eaac17
bbangjae <thebyunjae@naver.com>
2025-08-22 17:25:36 +0900

    pass(date): [PGS]_조건에 부합하는 중고거래 상태 조회하기

---
+++ programmers/date/[PGS]_조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,16 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/164672
+-- 조건에 부합하는 중고거래 상태 조회하기
+
+SELECT
+    BOARD_ID,
+    WRITER_ID,
+    TITLE,
+    PRICE,
+    CASE
+        WHEN STATUS = 'SALE' THEN '판매중'
+        WHEN STATUS = 'RESERVED' THEN '예약중'
+        ELSE '거래완료'
+        END STATUS
+FROM USED_GOODS_BOARD
+WHERE CREATED_DATE = '2022-10-5'
+ORDER BY BOARD_ID DESC
