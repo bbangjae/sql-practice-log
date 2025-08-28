e2056a2cbf60fad7c8035969ee221cbee94c62d2
bbangjae <thebyunjae@naver.com>
2025-08-28 23:54:01 +0900

    pass(group by): [PGS]_성분으로 구분한 아이스크림 총 주문량

---
+++ programmers/groupby/[PGS]_성분으로 구분한 아이스크림 총 주문량.sql
@@ -0,0 +1,9 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/133026
+-- 성분으로 구분한 아이스크림 총 주문량
+
+SELECT II.INGREDIENT_TYPE, SUM(TOTAL_ORDER) TOTAL_ORDER
+FROM FIRST_HALF FH
+    JOIN ICECREAM_INFO II
+        ON FH.FLAVOR = II.FLAVOR
+GROUP BY II.INGREDIENT_TYPE
+ORDER BY TOTAL_ORDER
