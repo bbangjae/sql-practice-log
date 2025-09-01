b10cd36c9a211ee4464131cc8a788495e7cfca01
bbangjae <thebyunjae@naver.com>
2025-09-01 23:58:21 +0900

    pass(select): [PGS]_특정 물고기를 잡은 총 수 구하기

---
+++ programmers/select/[PGS]_특정 물고기를 잡은 총 수 구하기.sql
@@ -0,0 +1,8 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/298518
+-- 특정 물고기를 잡은 총 수 구하기
+
+SELECT COUNT(*) AS FISH_COUNT
+FROM FISH_INFO AS I
+    JOIN FISH_NAME_INFO AS N
+        ON I.FISH_TYPE = N.FISH_TYPE
+WHERE N.FISH_NAME IN ('BASS','SNAPPER')
